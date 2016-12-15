0a89ae48aecfcfd2f6bf51e5f488461a55ca70dc
zsigmondszilveszter <zsigmondszilveszter@yahoo.com>
2016-12-15 08:27:08 +0200

    divided notification competents into 2 level of group

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const websiteurl = 'https://www.cashinfo.com';
 const timeout = 60000; // msec max timeout
-const to = 'szilveszter@onelogic.fr, zmathe@onelogic.fr, pmpeugnet@cashinfo.com';
+const to = 'szilveszter@onelogic.fr, zmathe@onelogic.fr';
+const to2 = 'pmpeugnet@cashinfo.com';
 
 const Curl = require( 'node-libcurl' ).Curl;
 const nodemailer = require('nodemailer');
@@ -11,15 +12,16 @@ let mailer = nodemailer.createTransport(conf.smtp_options)
 
 let mailOptions = {
     from: '"Szilveszter Zsigmond" <alert@onelogic.ro>', // sender address
-    to: to, // list of receivers
     subject: 'ALERT! '+websiteurl+' is down, timeout = '+parseInt(timeout/1000)+' sec', // Subject line
 };
 let counter = 0;
+let counter2 = 0;
 
-let sendEmail = text => {
+let sendEmail = (text, to) => {
 
     mailOptions.text = text; // plaintext body
     mailOptions.html = '<b>'+text+'</b>'; // html body
+    mailOptions.to = to
     mailer.sendMail(mailOptions, function(error, info){
         if(error){
             logger.writeLog('Message NOT sent ERROR: ' + error)
@@ -42,9 +44,11 @@ setInterval(()=>{
         logger.writeLog(text)
 
         if(statusCode == 404 || statusCode == 500){
-            sendEmail(text)
+            sendEmail(text, to)
         }
 
+        counter = 0;
+        counter2 = 0;
         this.close();
     });
 
@@ -52,9 +56,14 @@ setInterval(()=>{
         let date = new Date();
         let text = date.toLocaleString()+', ' + websiteurl+' is down, connection timeout set to '+parseInt(timeout/1000)+' sec'
 
-        if(++counter > 1){
+        if(++counter > 1){ // notify competents level 1 about the fall down
             counter = 0;
-            sendEmail(text)
+            sendEmail(text, to)
+        }
+
+        if(++counter2 > 4){ // notify competents level 2 about the fall down
+            counter2 = 0;
+            endEmail(text, to2)
         }
 
         logger.writeLog(text)
